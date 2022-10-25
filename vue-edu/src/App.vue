d4ce449be74bd9d96a325f917e1ddbeae7a7ddbb
batrakovd2 <batrakovd2@gmail.com>
2022-10-25 23:43:32 +0500

    posts begin

--- vue-edu/src/App.vue
+++ vue-edu/src/App.vue
@@ -1,9 +1,17 @@
 <template>
     <div>
-        <div>Like: {{ likes }}</div>
-        <div>Dislike: {{ dislikes }}</div>
-        <button>Кнопка like</button>
-        <button>Кнопка dislike</button>
+        <div class="post">
+            <strong>Название </strong>
+            <p>Пост о JS</p>    
+        </div>
+        <div class="post">
+            <strong>Название </strong>
+            <p>Пост о JS</p>    
+        </div>
+        <div class="post">
+            <strong>Название </strong>
+            <p>Пост о JS</p>    
+        </div>
     </div>
 </template>
 
@@ -14,11 +22,29 @@
                 likes: 0,
                 dislikes: 5,
             }
+        },
+        methods: {
+            addLike() {
+                this.likes += 1;
+            },
+            addDislike() {
+                this.dislikes += 1;
+            }
         }
     }
 </script>
 
 <style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
 
+.post{
+    padding: 15px;
+    border: 2px solid teal;
+    margin: 15px 20px;
+}
 </style>
 
